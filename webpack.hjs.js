2fe26715d3ac3174d35696f46da4742ec4df53c9
Carlos Villuendas <carlosvillu@gmail.com>
2016-08-04 21:20:09 +0200

    chore(deploy): Use surge intance of firebase

--- webpack.hjs.js
+++ webpack.hjs.js
@@ -10,36 +10,41 @@ const PUBLIC_PATH = isProd ? PRO_PUBLIC_PATH : DEV_PUBLIC_PATH
 const GA_ID = 'UA-XXXXX-Y'
 const GA_SCRIPT = isProd ? 'analytics' : 'analytics_debug'
 
+const template = (context) => {
+  return `
+    <html mode="${process.env.NODE_ENV || 'development'}">
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="${PUBLIC_PATH}${context.css}" rel="stylesheet">
+      </head>
+      <body>
+        <div id="root"></div>
+        <script deferred src="https://cdnjs.cloudflare.com/ajax/libs/localforage/1.4.2/localforage.nopromises.min.js"></script>
+        <script deferred src="https://www.gstatic.com/firebasejs/3.2.0/firebase.js"></script>
+        <script src="${PUBLIC_PATH}${context.vendor}"></script>
+        <script src="${PUBLIC_PATH}${context.main}"></script>
+        <script deferred src="https://apis.google.com/js/client.js?onload=OnLoadCallback"></script>
+        <script>
+          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+          ga('create', '${GA_ID}', 'auto');
+          ga('send', 'pageview');
+        </script>
+        <script async src="https://www.google-analytics.com/${GA_SCRIPT}.js"></script>
+      </body>
+    </html>
+  `
+}
+
 const config = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   html: context => {
     return {
-      'index.html': `
-        <html mode="${process.env.NODE_ENV || 'development'}">
-          <head>
-            <meta charset="utf-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="${PUBLIC_PATH}${context.css}" rel="stylesheet">
-          </head>
-          <body>
-            <div id="root"></div>
-            <script deferred src="https://cdnjs.cloudflare.com/ajax/libs/localforage/1.4.2/localforage.nopromises.min.js"></script>
-            <script deferred src="https://www.gstatic.com/firebasejs/3.2.0/firebase.js"></script>
-            <script src="${PUBLIC_PATH}${context.vendor}"></script>
-            <script src="${PUBLIC_PATH}${context.main}"></script>
-            <script deferred src="https://apis.google.com/js/client.js?onload=OnLoadCallback"></script>
-            <script>
-              window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-              ga('create', '${GA_ID}', 'auto');
-              ga('send', 'pageview');
-            </script>
-            <script async src="https://www.google-analytics.com/${GA_SCRIPT}.js"></script>
-          </body>
-        </html>
-      `
+      '200.html': template(context),
+      'index.html': template(context)
     }
   }
 })
